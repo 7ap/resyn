c74e4ce8bc442d36243bb8aa50a55b5f7dc6bc07
7ap <109433659+7ap@users.noreply.github.com>
2023-06-24 23:05:29 -0500

    feat: add speech synthesis
    uses the tts-server binary from "TTS" (coqui) python package

--- src/synthesis.rs
+++ src/synthesis.rs
@@ -1,10 +1,50 @@
+use std::env;
+use std::io::Cursor;
 use std::sync::mpsc::Receiver;
+use std::sync::Arc;
 use std::thread;
+use std::time::Instant;
+
+use reqwest::blocking::Client;
+use rodio::{Decoder, OutputStream, Sink};
 
 pub fn run(rx: Receiver<String>) {
-    while let Some(_speech) = rx.recv().ok() {
+    let client = Arc::new(Client::new());
+
+    #[rustfmt::skip]
+    let speaker_id = env::var("COQUI_SPEAKER_ID").expect("env variable `COQUI_SPEAKER_ID` is not set");
+
+    while let Some(speech) = rx.recv().ok() {
+        let client = client.clone();
+        let speaker_id = speaker_id.clone();
+
         thread::spawn(move || {
-            // TODO: Synthesize speech.
+            let now = Instant::now();
+            let speech = client
+                .get("http://[::1]:5002/api/tts")
+                .query(&[("text", speech), ("speaker_id", speaker_id)])
+                .send();
+
+            if speech.is_err() {
+                error!("failed to synthesize speech, is `tts-server` running?");
+                return;
+            }
+
+            let speech = speech.unwrap().bytes().unwrap().to_vec();
+            let source = Decoder::new(Cursor::new(speech.clone()));
+
+            if source.is_err() {
+                error!("failed to synthesize speech, is `COQUI_SPEAKER_ID` valid?");
+                return;
+            }
+
+            info!("synthesized {} bytes in {:?}", speech.len(), now.elapsed());
+
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            let sink = Sink::try_new(&stream_handle).unwrap();
+
+            sink.append(source.unwrap());
+            sink.sleep_until_end();
         });
     }
 }
